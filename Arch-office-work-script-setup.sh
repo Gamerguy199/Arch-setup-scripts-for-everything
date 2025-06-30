e5be5842e415b2bfc73cee3f36c6584d20792cdb
Stefan Ro123YT <121683781+Gamerguy199@users.noreply.github.com>
2025-06-30 13:52:11 +0000

    Arch-office-work-script-setup.sh
    Forgot to add "--noconfirm" to some comands 💀

--- Arch-office-work-script-setup.sh
+++ Arch-office-work-script-setup.sh
@@ -1,9 +1,9 @@
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 sudo pacman -S libreoffice-fresh plasma-meta sddm kitty fish nano  --noconfirm
 chsh -s /usr/bin/fish
 sudo systemctl enable sddm.service
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
-paru -S google-chrome
+paru -S google-chrome --noconfirm
 reboot
