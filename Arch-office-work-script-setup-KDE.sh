8470c3e68ba6557791c3ae8e1fa79a43698a7129
Stefan Ro123YT <121683781+Gamerguy199@users.noreply.github.com>
2025-08-06 12:09:34 +0000

    Arch-office-work-script-setup-KDE.sh

--- Arch-office-work-script-setup-KDE.sh
+++ Arch-office-work-script-setup-KDE.sh
@@ -1,6 +1,5 @@
 sudo pacman -Syu --noconfirm
-sudo pacman -S libreoffice-fresh plasma-meta sddm kitty fish nano  --noconfirm
-chsh -s /usr/bin/fish
+sudo pacman -S libreoffice-fresh plasma-desktop sddm kitty nano pavucontrol dolphin --noconfirm
 sudo systemctl enable sddm.service
 git clone https://aur.archlinux.org/paru.git
 cd paru
